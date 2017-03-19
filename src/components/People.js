fc05fc4371f5c6352d70e250f17102365b008ac2
mendelB <mendelblesofsky@gmail.com>
2017-03-19 19:04:42 -0400

    add tables and cells ugly static code

--- src/components/People.js
+++ src/components/People.js
@@ -7,22 +7,58 @@ class People extends Component {
     return (
       <div className="People">
       <div className="square">
-      	<PersonView/>
+      <div className="content">
+      <div className="table">
+      <div className="table-cell"> 
+      	<PersonView name="mendel" city="nyc"/>
+      </div>
+      </div>
+      </div>
       </div>
       <div className="square">
-      	<PersonView/>
+      <div className="content">
+      <div className="table">
+      <div className="table-cell"> 
+      	<PersonView name="John" city="nyc"/>
+      </div>
+      </div>
+      </div>
       </div>
       <div className="square">
-      	<PersonView/>
+      <div className="content">
+      <div className="table">
+      <div className="table-cell"> 
+      	<PersonView name="Sara" city="nyc"/>
+      </div>
+      </div>
+      </div>
       </div>
       <div className="square">
-      	<PersonView/>
+      <div className="content">
+      <div className="table">
+      <div className="table-cell"> 
+      	<PersonView name="Donna" city="nyc"/>
+      </div>
+      </div>
+      </div>
       </div>
       <div className="square">
-      	<PersonView/>
+      <div className="content">
+      <div className="table">
+      <div className="table-cell"> 
+      	<PersonView name="Kevin" city="nyc"/>
+      </div>
+      </div>
+      </div>
       </div>
       <div className="square">
-      	<PersonView/>
+      <div className="content">
+      <div className="table">
+      <div className="table-cell"> 
+      	<PersonView name="Hillary" city="nyc"/>
+      </div>
+      </div>
+      </div>
       </div>
       </div>
     );
