201b84eeffb6748e1d8a3706ac1ecfdaa5254933
mendelB <mendelblesofsky@gmail.com>
2017-03-19 19:57:33 -0400

    load api data of people

--- src/components/People.js
+++ src/components/People.js
@@ -3,64 +3,55 @@ import '../App.css';
 import PersonView from './PersonView'
 
 class People extends Component {
+	constructor(props) {
+		super(props);
+		this.fetchPeople = this.fetchPeople.bind(this)
+
+		this.state = {
+			loaded: false,
+			people: []
+		}
+	}
+
+	componentDidMount() {
+		this.fetchPeople()
+		.then(json => {
+			this.setState({people: json, loaded: true})
+		})
+	}
+
+	fetchPeople() {
+		return fetch('http://spotify-people-api.herokuapp.com/')
+		.then(function(response) {
+  		if(response.ok) {
+    		return response.json();
+  		} else {
+  			throw new Error('Network response was not ok.')
+  		}
+		})
+	}
+
   render() {
+  	var people = this.state.people.map((person, index) => {
+      return (<div key={index} className="square">
+	      <div className="content">
+		      <div className="table">
+			      <div className="table-cell"> 
+			      	<PersonView name={person.name} city={person.favorite_city}/>
+			      </div>
+			    </div>
+	      </div>
+      </div>)
+  	})
     return (
-      <div className="People">
-      <div className="square">
-      <div className="content">
-      <div className="table">
-      <div className="table-cell"> 
-      	<PersonView name="mendel" city="nyc"/>
-      </div>
-      </div>
-      </div>
-      </div>
-      <div className="square">
-      <div className="content">
-      <div className="table">
-      <div className="table-cell"> 
-      	<PersonView name="John" city="nyc"/>
-      </div>
-      </div>
-      </div>
-      </div>
-      <div className="square">
-      <div className="content">
-      <div className="table">
-      <div className="table-cell"> 
-      	<PersonView name="Sara" city="nyc"/>
-      </div>
-      </div>
-      </div>
-      </div>
-      <div className="square">
-      <div className="content">
-      <div className="table">
-      <div className="table-cell"> 
-      	<PersonView name="Donna" city="nyc"/>
-      </div>
-      </div>
-      </div>
-      </div>
-      <div className="square">
-      <div className="content">
-      <div className="table">
-      <div className="table-cell"> 
-      	<PersonView name="Kevin" city="nyc"/>
-      </div>
-      </div>
-      </div>
-      </div>
-      <div className="square">
-      <div className="content">
-      <div className="table">
-      <div className="table-cell"> 
-      	<PersonView name="Hillary" city="nyc"/>
-      </div>
-      </div>
-      </div>
-      </div>
-      </div>
+	      this.state.loaded ?
+      	<div className="People">
+	      	{people}
+	      </div>
+	      :
+	      <div className="People">
+	      	Loading...
+      	</div>
     );
   }
 }
