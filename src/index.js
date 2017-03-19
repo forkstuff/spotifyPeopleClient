95db43cdfbad714242da4992a841a2f2f284aad9
mendelB <mendelblesofsky@gmail.com>
2017-03-19 18:27:35 -0400

    fix component loading

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import App from './App';
-import People from './People';
+import People from './components/People';
+import PersonView from './components/PersonView';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
