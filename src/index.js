58af7c5c7732d65198f67c83457cb5efac728a07
mendelB <mendelblesofsky@gmail.com>
2017-03-19 17:15:38 -0400

    configure routes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
 import App from './App';
+import People from './People';
 import './index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
 
 ReactDOM.render(
-  <App />,
+  <Router history={browserHistory}>
+  	<Route path="/" component={App}>
+  		<IndexRoute component={People}/>
+  	</Route>
+  </Router>,
   document.getElementById('root')
 );
