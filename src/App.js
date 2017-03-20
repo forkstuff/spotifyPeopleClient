6a18ca9963e571fc9f61e96ae712bf3a9863128a
mendelB <mendelblesofsky@gmail.com>
2017-03-19 20:04:52 -0400

    add create person button

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
+import { browserHistory } from 'react-router';
+import {Button} from 'react-bootstrap'
 import logo from './logo.png';
 import './App.css';
 
 class App extends Component {
+  handleClick() {
+    const path = '/create-person'
+      browserHistory.push(path)
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2 className="App-Header-Text">Welcome to Spotify-People</h2>
+          <Button className="CreatePersonButton" bsStyle="primary" onClick={this.handleClick}>Create New Person</Button>
         </div>
         { this.props.children }
       </div>
