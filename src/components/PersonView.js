13f5ef64a578c02a5da03cd3d3b0ccc3ede9f6be
mendelB <mendelblesofsky@gmail.com>
2017-03-19 21:14:41 -0400

    person view will fetch data if no params given

--- src/components/PersonView.js
+++ src/components/PersonView.js
@@ -3,17 +3,39 @@ import {Button} from 'react-bootstrap'
 import '../App.css';
 
 class PersonView extends Component {
-	// componentDidMount() {
-	// 	if (!this.props.hasData) {
-	// 		this.fetchData(this.props.routeParams.id)
-	// 	}
-	// }
+	constructor(props) {
+		super(props);
+		
+		this.state = {
+			name: '',
+			city: ''
+		}
+	}
+	componentDidMount() {
+		if (!this.props.hasData) {
+			this.fetchPerson(this.props.routeParams.id)
+			.then(response => {
+				this.setState({name: response.name, city: response.favorite_city})
+			})
+		}
+	}
+
+	fetchPerson(id) {
+		return fetch('http://spotify-people-api.herokuapp.com/people/' + id)
+		.then(function(response) {
+  		if(response.ok) {
+    		return response.json();
+  		} else {
+  			throw new Error('Network response was not ok.')
+  		}
+		})
+	}
 
   render() {
     return (
       <div className="PersonView">
-      	<div>Name: {this.props.name}</div>
-      	<div>Favorite City: {this.props.city}</div>
+      	<div>Name: {this.props.name || this.state.name}</div>
+      	<div>Favorite City: {this.props.city || this.state.city}</div>
       	<Button bsSize="small" bsStyle="warning">Edit</Button>{' '}
       	<Button bsSize="small" bsStyle="danger">Delete</Button>
       </div>
