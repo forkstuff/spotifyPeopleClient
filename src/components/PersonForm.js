393612bff76d6e9ba18143c4f1f2b548df438028
mendelB <mendelblesofsky@gmail.com>
2017-03-20 02:08:10 -0400

    listen to submit for form

--- src/components/PersonForm.js
+++ src/components/PersonForm.js
@@ -17,7 +17,7 @@ class PersonForm extends Component {
 
 		this.handleChangeName = this.handleChangeName.bind(this);
 		this.handleChangeCity = this.handleChangeCity.bind(this);
-		this.handleClick = this.handleClick.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
 		this.getValidationState = this.getValidationState.bind(this);
 	}
 
@@ -53,7 +53,8 @@ class PersonForm extends Component {
 	  return (val1.length >= 1 && val2.length >= 1)
   }
 
-	handleClick() {
+	handleSubmit(e) {
+		e.preventDefault()
 		if (this.state.editForm) {
 			dataFetcher.editPerson(this.state)
 			.then(response => {
@@ -73,12 +74,14 @@ class PersonForm extends Component {
       	<div className="Form">
 					<h1>Person Form</h1>
 					<div className="Input">
-						<FormGroup bsSize="large">
-	      					<FormControl type="text" placeholder="Input Name Here" value={this.state.name} onChange={this.handleChangeName} /><br/>
-	      					<FormControl type="text" placeholder="Input Favorite City Here" value={this.state.favoriteCity} onChange={this.handleChangeCity} />
-	    				</FormGroup>
-	    			</div>
-					<Button bsStyle="success" bsSize="large" onClick={this.handleClick} disabled={!this.state.valid}>Submit!</Button>
+						<form onSubmit={this.handleSubmit}>
+							<FormGroup bsSize="large">
+		      			<FormControl type="text" placeholder="Input Name Here" value={this.state.name} onChange={this.handleChangeName} /><br/>
+		      			<FormControl type="text" placeholder="Input Favorite City Here" value={this.state.favoriteCity} onChange={this.handleChangeCity} />
+		    			</FormGroup>
+							<Button type="submit" bsStyle="success" bsSize="large" onClick={this.handleSubmit} disabled={!this.state.valid}>Submit!</Button>
+		    		</form>
+		    	</div>
 				</div>
       </div>
     );
