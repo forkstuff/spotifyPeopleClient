e77a432c7b7bbe6ac246fc3d3db102b387a1d2ff
mendelB <mendelblesofsky@gmail.com>
2017-03-20 00:52:34 -0400

    remove excess code

--- src/components/PersonView.js
+++ src/components/PersonView.js
@@ -1,43 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router';
 import {Button} from 'react-bootstrap'
 import '../App.css';
 
 class PersonView extends Component {
-	constructor(props) {
-		super(props);
-		
-		this.state = {
-			name: '',
-			city: ''
-		}
-	}
-	componentDidMount() {
-		if (!this.props.hasData) {
-			this.fetchPerson(this.props.routeParams.id)
-			.then(response => {
-				this.setState({name: response.name, city: response.favorite_city})
-			})
-		}
-	}
-
-	fetchPerson(id) {
-		return fetch('http://spotify-people-api.herokuapp.com/people/' + id)
-		.then(function(response) {
-  		if(response.ok) {
-    		return response.json();
-  		} else {
-  			throw new Error('Network response was not ok.')
-  		}
-		})
-	}
-
   render() {
     return (
       <div className="PersonView">
-      	<div className="Info">Name: {this.props.name || this.state.name}</div>
-      	<div className="Info">Favorite City: {this.props.city || this.state.city}</div>
-      	<Button bsSize="small" bsStyle="warning">Edit</Button>{' '}
-      	<Button bsSize="small" bsStyle="danger">Delete</Button>
+      	<div className="Info">Name: {this.props.name}</div>
+      	<div className="Info">Favorite City: {this.props.city}</div>
+      	<Button bsSize="small" bsStyle="warning" onClick={this.props.onClickEdit}>Edit</Button>{' '}
+      	<Button bsSize="small" bsStyle="danger" onClick={this.props.onClickDelete}>Delete</Button>{' '}
+      	<Link to={"/people/" + this.props.id}><Button bsSize="small" bsStyle="primary">Main View</Button></Link>
       </div>
     );
   }
